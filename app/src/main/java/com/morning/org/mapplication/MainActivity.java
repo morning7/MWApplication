6045c94891d9a6078f23afb496eadc109d9241a3
morning <272613245@qq.com>
2016-05-26 16:07:19 +0800

    Initial commit

---
+++ app/src/main/java/com/morning/org/mapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.morning.org.mapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.textview)).setText("第一个github测试");
+    }
+
+}
